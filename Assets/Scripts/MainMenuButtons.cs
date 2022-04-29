54d53f5f42f714d9d459080a80d2c485422e9eec
graanton <graanton5@gamil.com>
2022-04-29 18:45:57 +0700

    Небольшие изменения в главном меню

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -5,8 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    [SerializeField] private GameObject _mainMenuPanel;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private float _swipeSpeed;
+    [SerializeField] private Transform _changePlayerPoint;
+
     public void LoadMainScene(int mainScene)
     {
         SceneManager.LoadScene(mainScene);
+    }
+    public void StartGoingToChangePlayers()
+    {
+        DisableMenuPanel();
+        StartCoroutine(GoingToChangePlayer());
+    }
+    
+    private void DisableMenuPanel()
+    {
+        _mainMenuPanel.SetActive(true);
+    }
+
+    private void EnableMenuPanel()
+    {
+        _mainMenuPanel.SetActive(false);
+    }
+
+    private IEnumerator GoingToChangePlayer()
+    {
+        while (Vector3.Distance(_camera.position, _changePlayerPoint.position) != 0)
+        {
+            _camera.position = Vector3.Lerp(_camera.position, _changePlayerPoint.position, _swipeSpeed * Time.fixedDeltaTime);
+            yield return null;
+        }
+        
     }
 }
