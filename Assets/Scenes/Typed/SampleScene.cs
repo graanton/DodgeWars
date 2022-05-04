7c22620537e12cab4f9f93ab9d8aa726e73022a8
NotSpacePhetos <104357491+NotSpacePhetos@users.noreply.github.com>
2022-05-04 18:29:00 +0700

    Две новые модели с кривыми скелетами, но рабочими!

--- Assets/Scenes/Typed/SampleScene.cs
+++ Assets/Scenes/Typed/SampleScene.cs
@@ -18,14 +18,14 @@ namespace IJunior.TypedScenes
         
         private const string _sceneName = "SampleScene";
         
-        public static void Load(UnityEngine.GameObject argument, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        public static void Load(LoadSceneMode loadSceneMode = LoadSceneMode.Single)
         {
-            LoadScene(_sceneName, loadSceneMode, argument);
+            LoadScene(_sceneName, loadSceneMode);
         }
         
-        public static UnityEngine.AsyncOperation LoadAsync(UnityEngine.GameObject argument, LoadSceneMode loadSceneMode = LoadSceneMode.Single)
+        public static UnityEngine.AsyncOperation LoadAsync(LoadSceneMode loadSceneMode = LoadSceneMode.Single)
         {
-            return LoadScene(_sceneName, loadSceneMode, argument);
+            return LoadScene(_sceneName, loadSceneMode);
         }
     }
 }
